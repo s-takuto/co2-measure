bfcd6152dfa7a5a6853eb3df12e9b9cea30e5bc9
s_takuto <ts17.ngm@gmail.com>
2021-02-12 18:32:41 +0900

    [add] Lチカスクリプトを追加

---
+++ src/light.py
@@ -0,0 +1,11 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(2, GPIO.OUT)
+
+while True:
+    GPIO.output(2, GPIO.HIGH)
+    time.sleep(0.5)
+    GPIO.output(2, GPIO.LOW)
+    time.sleep(0.5)
