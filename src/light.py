5bcdd3450c950520959fa94930cef981e059ca4a
s_takuto <ts17.ngm@gmail.com>
2021-02-12 18:44:20 +0900

    [update] 同時に2個Lチカする機能を追加

--- src/light.py
+++ src/light.py
@@ -3,9 +3,12 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(2, GPIO.OUT)
+GPIO.setup(4, GPIO.OUT)
 
 while True:
     GPIO.output(2, GPIO.HIGH)
+    GPIO.output(4, GPIO.HIGH)
     time.sleep(0.5)
     GPIO.output(2, GPIO.LOW)
+    GPIO.output(4, GPIO.LOW)
     time.sleep(0.5)
