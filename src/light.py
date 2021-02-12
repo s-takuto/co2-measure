74b4dc34305a040624767286d8e3661bbb4d2d57
s_takuto <ts17.ngm@gmail.com>
2021-02-12 19:01:36 +0900

    [update]コメントを追加

--- src/light.py
+++ src/light.py
@@ -1,14 +1,20 @@
 import RPi.GPIO as GPIO
 import time
 
+# GPIO.BOARD:=物理ピン番号、GPIO.BCM:=役割ピン番号
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(2, GPIO.OUT)
 GPIO.setup(4, GPIO.OUT)
 
 while True:
+    # HIGHは点灯
     GPIO.output(2, GPIO.HIGH)
     GPIO.output(4, GPIO.HIGH)
+
     time.sleep(0.5)
+
+    # LOWは消灯
     GPIO.output(2, GPIO.LOW)
     GPIO.output(4, GPIO.LOW)
+
     time.sleep(0.5)
